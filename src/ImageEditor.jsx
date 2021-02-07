dca9e57a1ef0d23d3c9a1518355d94545457a73f
Johan Winge <johan.winge@gmail.com>
2021-02-07 01:54:43 +0100

    Various display modes.

--- src/ImageEditor.jsx
+++ src/ImageEditor.jsx
@@ -3,28 +3,41 @@ import useImage from 'use-image';
 import boxes from './boxes.json';
 import font from './font.json';
 
-// Blend a square from source1 with source2 at displacement, and paste result in target 
-function blend(source1, xpos, ypos, width, height, source2, xdisp, ydisp, mode, target) {
-  const srcData1 = source1.getContext('2d').getImageData(xpos, ypos, width, height);
-  const srcData2 = source2.getContext('2d').getImageData(xdisp, ydisp, width, height);
-  const targetData = target ? target.getContext('2d').getImageData(xdisp, ydisp, width, height) : srcData2;
-  const len = srcData1.data.length;
+// Blend a square from template with target at displacement, and paste result in destination
+function blend(source, xpos, ypos, width, height, target, xdisp, ydisp, mode, dest) {
+  const sourceData = source.getContext('2d').getImageData(xpos, ypos, width, height);
+  const targetData = target.getContext('2d').getImageData(xdisp, ydisp, width, height);
+  const destData = dest ? dest.getContext('2d').getImageData(xdisp, ydisp, width, height) : targetData;
+  const len = sourceData.data.length;
   for (let px = 0; px < len; px += 4) {
-    const a = 1.0 - (srcData1.data[px] + srcData1.data[px+1] + srcData1.data[px+2]) / (3 * 255);
-    const b = 1.0 - (srcData2.data[px] + srcData2.data[px+1] + srcData2.data[px+2]) / (3 * 255);
+    const s = 1.0 - sourceData.data[px+1] / 255;
+    const t = 1.0 - targetData.data[px+1] / 255;
     if (mode === 'diff') {
-      targetData.data[px]   = 255 * (1.0 - 1.0*a*a - 1.0*b*b + 2.0*a*b);
-      targetData.data[px+1] = 255 * (1.0 - 1.0*a*a - 0.6*b*b + 1.6*a*b);
-      targetData.data[px+2] = 255 * (1.0 - 0.6*a*a - 1.0*b*b + 1.6*a*b);
+      const sRed= 1.0 - sourceData.data[px] / 255;
+      const redDiff = s - sRed;
+      const ss = s * s;
+      const tt = t * t;
+      const st = s * t;
+      destData.data[px]   = (1.0 - redDiff) * 255 * (1.0 - 1.0 * ss - 1.0 * tt + 2.0 * st) + redDiff * sourceData.data[px];
+      destData.data[px+1] = (1.0 - redDiff) * 255 * (1.0 - 1.0 * ss - 0.6 * tt + 1.6 * st) + redDiff * sourceData.data[px+1];
+      destData.data[px+2] = (1.0 - redDiff) * 255 * (1.0 - 0.6 * ss - 1.0 * tt + 1.6 * st) + redDiff * sourceData.data[px+2];
     } else if (mode === 'maxink') {
-      const result = 255 * (1.0 - Math.max(a, b));
-      targetData.data[px] = result;
-      targetData.data[px+1] = result;
-      targetData.data[px+2] = result;
+      const result = 255 * (1.0 - Math.max(s, t));
+      destData.data[px] = result;
+      destData.data[px+1] = result;
+      destData.data[px+2] = result;
+    } else if (mode === 'overlapwarn') {
+      const tRed = 1.0 - targetData.data[px] / 255;
+      const adaptedSum = s + 2 * t - tRed;
+      const d = Math.min(1.0, adaptedSum);
+      const dRed = 2 * d - Math.min(2.0, adaptedSum);
+      destData.data[px] = 255 * (1.0 - dRed);
+      destData.data[px+1] = 255 * (1.0 - d);
+      destData.data[px+2] = 255 * (1.0 - d);
     }
-    targetData.data[px+3] = 255;
+    destData.data[px+3] = 255;
   }
-  (target ? target : source2).getContext('2d').putImageData(targetData, xdisp, ydisp);
+  (dest ? dest : target).getContext('2d').putImageData(destData, xdisp, ydisp);
 }
 
 function useImageCanvas(url) {
@@ -52,10 +65,11 @@ function ImageEditor({ url }) {
   const frontCanvasRef = useRef();
   const pageCanvas = useImageCanvas(url);
   const fontCanvas = useImageCanvas('font.png');
+  const [displayMode, setDisplayMode] = useState('page');
+  const [warnOverlap, setWarnOverlap] = useState(true);
   const [scaleFactor, setScaleFactor] = useState(0.5);
   const [selectedBox, setSelectedBox] = useState(null);
   const [draggingBox, setDraggingBox] = useState(null);
-
 
   const coords = (event) => {
     const rect = event.target.getBoundingClientRect(); 
@@ -64,7 +78,7 @@ function ImageEditor({ url }) {
     return {x, y};
   };
 
-  const redrawCanvas = useCallback(() => {
+  const redrawBackCanvas = useCallback(() => {
     if (pageCanvas && fontCanvas) {
       const { width, height } = pageCanvas;
 
@@ -73,28 +87,36 @@ function ImageEditor({ url }) {
       typedCanvas.height = height;
       typedCanvas.getContext('2d').fillStyle = "#FFFFFF";
       typedCanvas.getContext('2d').fillRect(0, 0, width, height);
-
       for (const box of boxes) {
         const fontbox = font[box.s][box.c];
         if (fontbox) {
           const {x, y, w, h} = fontbox;
-          blend(fontCanvas, x, y, w, h, typedCanvas, box.x, box.y, 'maxink');
+          blend(fontCanvas, x, y, w, h, typedCanvas, box.x, box.y, warnOverlap ? 'overlapwarn' : 'maxink');
         }
       }
 
       const blendCanvas = document.createElement('canvas');
       blendCanvas.width = width;
       blendCanvas.height = height;
-
       blend(typedCanvas, 0, 0, width, height, pageCanvas, 0, 0, 'diff', blendCanvas);
 
       const ctx = backCanvasRef.current.getContext('2d');
       ctx.setTransform(1, 0, 0, 1, 0, 0);
       ctx.clearRect(0, 0, width, height);
       ctx.scale(scaleFactor * dpr, scaleFactor * dpr);
-      ctx.drawImage(blendCanvas, 0, 0);
+      if (displayMode === 'page') {
+        ctx.drawImage(pageCanvas, 0, 0);        
+      } else if (displayMode === 'type') {
+        ctx.drawImage(typedCanvas, 0, 0);        
+      } else if (displayMode === 'diff') {
+        ctx.drawImage(blendCanvas, 0, 0);        
+      } 
     }
-  }, [fontCanvas, pageCanvas, scaleFactor]);
+  }, [fontCanvas, pageCanvas, scaleFactor, displayMode, warnOverlap]);
+
+  useEffect(() => {
+    redrawBackCanvas();
+  }, [redrawBackCanvas]);
 
   const handleMouseDown = (event) => {
     const {x, y} = coords(event);
@@ -133,14 +155,10 @@ function ImageEditor({ url }) {
       const {x, y} = coords(event);
       selectedBox.box.x = x - draggingBox.xdisp;
       selectedBox.box.y = y - draggingBox.ydisp;
-      redrawCanvas();
+      redrawBackCanvas();
       setDraggingBox(null);
     }
   };
-
-  useEffect(() => {
-    redrawCanvas();
-  }, [redrawCanvas, pageCanvas, fontCanvas, scaleFactor]);
 
   useEffect(() => {
     const ctx = frontCanvasRef.current.getContext('2d');
@@ -161,6 +179,10 @@ function ImageEditor({ url }) {
 
   return (
     <div className="ImageEditor">
+      <button onClick={() => setDisplayMode('page')}>Page</button>
+      <button onClick={() => setDisplayMode('type')}>Type</button>
+      <button onClick={() => setDisplayMode('diff')}>Diff</button>
+      <button onClick={() => setWarnOverlap(!warnOverlap)}>Overlap warning</button>
       {
         [0.25, 0.5, 1.0].map(scale => (
           <button
